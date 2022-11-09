80ff01b06e14254e2bfc614be9d5dfe21c294ff4
dariglad <DariGlad>
2022-11-09 21:52:38 +0500

    first push

---
+++ ads/serializers/__init__.py
@@ -0,0 +1,9 @@
+from .ad import AdListSerializer, AdDetailSerializer, AdSerializer
+from .category import CategorySerializer
+
+__all__ = [
+    "AdListSerializer",
+    "AdDetailSerializer",
+    "AdSerializer",
+    "CategorySerializer"
+]
